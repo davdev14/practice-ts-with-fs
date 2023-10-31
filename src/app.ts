8a15fbbc03aad828a2be657ba50f95c127198d25
davdev14 <davdev1400@gmail.com>
2023-11-01 00:35:58 +0100

    can create files and folders

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-import fs from "node:fs"; 
+import fs from "node:fs";
+import path from "node:path";
+
+const folderPath = '/home/david-ouatedem/Documents/my-folder';
+const filePath = path.join(folderPath, 'example.txt');
+const content = 'This is the content of the file.';
+
+fs.mkdir(folderPath, { recursive: true }, (err) => {
+  if (err || fs.existsSync(filePath)) {
+    console.error('Error creating the folder:', err);
+  } else {
+    fs.writeFile(filePath, content, (err) => {
+      if (err) {
+        console.error('Error creating the file:', err);
+      } else {
+        console.log('Folder and file created successfully.');
+      }
+    });
+  }
+});
