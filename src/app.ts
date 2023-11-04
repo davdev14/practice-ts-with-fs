94cd809224f3919e357653908c00db6f1ed1ce16
davdev14 <davdev1400@gmail.com>
2023-11-04 21:00:32 +0100

    filter done

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,50 @@
 import fs from "node:fs";
-import path from "node:path";
 
-const folderPath = '/home/david-ouatedem/Documents/my-folder';
-const filePath = path.join(folderPath, 'example.txt');
-const content = 'This is the content of the file.';
-
-fs.mkdir(folderPath, { recursive: true }, (err) => {
-  if (err || fs.existsSync(filePath)) {
-    console.error('Error creating the folder:', err);
-  } else {
-    fs.writeFile(filePath, content, (err) => {
-      if (err) {
-        console.error('Error creating the file:', err);
-      } else {
-        console.log('Folder and file created successfully.');
-      }
-    });
+const handleCreateFolder = (folderPath: string) => {
+  try {
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath);
+      console.log("folder created");
+    } else {
+      console.log("failed to create folder");
+    }
+  } catch (error) {
+    console.log(error);
   }
-});
+};
+// handleCreateFolder('/home/david-ouatedem/Documents/test');
+
+const handleCreateFile = (filePath: string, content: string) => {
+  try {
+    fs.writeFileSync(filePath, content);
+    console.log("file created");
+  } catch (error) {
+    console.log(error);
+  }
+};
+// handleCreateFile('/home/david-ouatedem/Documents/test/test-file.txt', 'my first file');
+
+const sortFromZToA = (b: any, a: any) => {
+  return b.localeCompare(a)
+};
+
+
+const handleFilterDirectoriesInADirectory = (source: string) => {
+  try {
+    const directoriesEntry = fs.readdirSync(source, { withFileTypes: true });
+
+    const directories = directoriesEntry
+      .filter((dir) => dir.isDirectory())
+      .map((dir) => dir.name);
+      console.log(directories);
+      
+
+    const sortedDirectories = directories.sort((a, b) => b.localeCompare(a));
+    console.log('Directories (Z to A):', sortedDirectories);    
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+
+handleFilterDirectoriesInADirectory("/home/david-ouatedem/Documents");
